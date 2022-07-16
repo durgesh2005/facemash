f796d44d6ec7bd606dada5f103c9d146585a7ecc
durgesh2005 <108167922+durgesh2005@users.noreply.github.com>
2022-07-16 23:07:03 +0530

    Add files via upload

---
+++ facemash.js
@@ -0,0 +1,31 @@
+$('.red-heart').hide();
+$(document).ready(function(){
+$('.heart').click(function(){
+    $('.red-heart').toggle();
+});
+});
+$(document).ready(function(){
+$('.red-heart').click(function(){
+$('.red-heart').toggle();
+});
+});
+
+$('.red-heart2').hide();
+$(document).ready(function(){
+$('.heart2').click(function(){
+    $('.red-heart2').toggle();
+});
+});
+$(document).ready(function(){
+$('.red-heart2').click(function(){
+$('.red-heart2').toggle();
+});
+});
+
+$('.heart').click(function(){
+$('.red-heart2').hide();
+});
+
+$('.heart2').click(function(){
+ $('.red-heart').hide();
+ });
